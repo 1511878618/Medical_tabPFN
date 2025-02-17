7b412c45770d5933167e05c7fff0fce7f494a202
big_xutingfeng <13984376+big-xutingfeng@user.noreply.gitee.com>
2025-02-17 23:04:06 +0800

    update code

--- V4/data/config.py
+++ V4/data/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # project_dir = Path("V3")
-project_dir = Path("V1")
+project_dir = Path("V4")
 project_dir.mkdir(exist_ok=True, parents=True)
 
 outputDir = project_dir / "output"
@@ -9,3 +9,10 @@ dataDir = project_dir / "data"
 
 outputDir.mkdir(exist_ok=True)
 dataDir.mkdir(exist_ok=True)
+
+# config output dir
+assocDir = outputDir / "01-assoc"
+riskModelDir = outputDir / "02-risk-model"
+
+assocDir.mkdir(exist_ok=True)
+riskModelDir.mkdir(exist_ok=True)
